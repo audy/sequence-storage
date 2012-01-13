39bd49dc9ae21999648a470794d84dc5c2ddff53
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-13 15:17:59 -0500

    beginnings of a user model

--- environment.rb
+++ environment.rb
@@ -2,6 +2,7 @@ require 'sinatra' unless defined?(Sinatra)
 require 'dm-core'
 require 'dm-migrations'
 require 'dm-validations'
+require 'bcrypt'
 require './models.rb'
 
 ENVIRONMENT = :deveopment
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -26,3 +26,33 @@ describe 'Experiment Model' do
   end
   
 end
+
+describe 'User Model' do
+  
+  let(:user) { User.new(:email => 'test@test.com') }
+  
+  it 'can be created' do
+    user.should_not be_nil
+  end
+  
+  it 'encrypts password' do
+    user.password = 'test_password'
+    user.save
+    user.crypted_password.should_not be_nil
+  end
+  
+  it 'can be authenticated' do
+    user.password = 'test_password'
+    user.save
+    user = User.authenticate('test@test.com', 'test_password')
+    user.should_not be_nil
+  end
+  
+  it 'can reject bad authentication' do
+    user.password = 'test_password'
+    user.save
+    user = User.authenticate('test@user.com', 'this is not the password')
+    user.should be_nil
+  end
+  
+end
