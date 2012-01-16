f8c33bd30001da025e1dbd00ba246c17a72d7e0d
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-16 14:02:07 -0500

    made gravatars better

--- application.rb
+++ application.rb
@@ -1,13 +1,23 @@
 require './environment.rb'
 
 helpers do
+  
+  # compose a link tag
   def link_tag(args={})
     $stderr.puts
     "<a href=\"#{args[:to]}\"" + " class=\"#{args[:class]}\" " + ">#{args[:name]}</a>"
   end
   
+  # pretty time format
   def pretty_time(t)
     t.strftime("%m/%d/%y")
+  end
+  
+  # returns url for gravatar
+  def gravatar(args={})
+    email = args[:email]
+    size = args[:size] || 100
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}"
   end
 end
 
