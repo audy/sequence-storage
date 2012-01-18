01a958eba3c3351f899fc4689c3395456924cae6
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-18 00:32:29 -0500

    cleaned up environment

--- environment.rb
+++ environment.rb
@@ -1,13 +1,21 @@
-require 'sinatra' unless defined?(Sinatra)
+# Gems
+require 'sinatra'
 require 'dm-core'
 require 'dm-migrations'
 require 'dm-validations'
 require 'dm-timestamps'
 require 'bcrypt'
 
+# Local stuff
 require './models.rb'
+
+# Setup environment
+# ENV can be :development or :production
+# This isn't really being used at the moment
+ENVIRONMENT = ENV['SINATRA_ENV'] || :development
+
+# Setup Database
 DataMapper.finalize
-ENVIRONMENT = :development
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://' + File.expand_path(File.dirname(__FILE__)) + '/db/development.db')
 
 enable :sessions
