f5fdf235f60d1631ed6b669ee376d6f49c9e79af
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-18 14:45:36 -0500

    added a simple search tool

--- application.rb
+++ application.rb
@@ -161,6 +161,18 @@ get '/session/destroy' do
 end
 
 ##
+# Searching
+#
+get '/search/?' do
+  $stderr.puts params
+  query = params[:q]
+  @results = Set.new
+  @results = (User.all(:name.like => query) | User.all(:email.like => query)).to_set + (Experiment.all(:name.like => query) | Experiment.all(:description.like => query)).to_set
+  erb :search
+end
+
+
+##
 # Uploading/Downloading
 get '/upload' do
   erb :upload
