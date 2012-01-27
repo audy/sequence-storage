1fd084671e7ccd887397db1a5aa2d3aa6df60f96
Yushun <sonnyhe2002@gmail.com>
2012-01-27 16:10:53 -0500

    add 2 more tests

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -40,7 +40,56 @@ describe 'User pages' do
     visit '/user/1'
     response_body.should contain user.name
   end
+  
+  it 'should not display user information when not logged in' do
+    visit '/user/1'
+    response_body.should_not contain user.name
+  end
 end
 
-describe '/experiments' do
+describe 'Experiment pages' do
+  
+  let (:user) { User.create :name => "test_user", :email => "test@test.com", :password => 'test' }
+  
+  let (:login!) {
+    visit '/session/new'
+    fill_in 'email', :with => user.email
+    fill_in 'password', :with => user.password
+    click_button "Login"
+  }
+  
+  let (:experiment){Experiment.create :name => "test_experiment", :description => "test_description"}
+  
+  it 'should list experiments' do
+    login!
+    experiment.users << user
+    experiment.save
+    
+    visit '/experiments'
+    response_body.should contain experiment.name
+  end
+  
+  it 'should be able to login' do
+    login!
+    visit '/experiments'
+    response_body.should contain "Logout"
+  end
+  
+  it 'should not create experiments without name' do
+    login!
+    visit '/experiment/new'
+    fill_in 'description', :with => "This experiment should not be created"
+    click_button "Save"
+    response_body.should_not contain experiment.name
+  end
+ 
+  ## does not pass
+  #it 'should create experiment' do
+  #  login!
+  #  visit '/experiment/new'
+  #  fill_in 'name', :with => "test_experiment"
+  #  fill_in 'description', :with => "This experiment should be created"
+  #  click_button "Save"
+  #  response_body.should contain "test_experiment"
+  #end
 end
