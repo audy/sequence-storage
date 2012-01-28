feb08068aceabfd48706f8a018c2e6196d33001a
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-28 10:19:26 -0500

    rearranged

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,24 +3,22 @@ require File.join(File.dirname(__FILE__), '..', 'application')
 require 'rack/test'
 require 'webrat'
 
-set :environment, :test
 set :logging, false
-
-# Setup in-memory database for testing.
-DataMapper.setup(:default, "sqlite3::memory:")
 
 Webrat.configure do |config|
   config.mode = :rack
 end
 
 RSpec.configure do |config|
-  
-  def app
-    Sinatra::Application
-  end
-  
-  config.before(:each) { DataMapper.auto_migrate! }
   config.include Rack::Test::Methods
   config.include Webrat::Matchers
   config.include Webrat::Methods
+  
+  config.filter_run_excluding :broken => true
+  
+  config.before(:each) { DataMapper.auto_migrate! }
+end
+
+def app
+  Sinatra::Application
 end
