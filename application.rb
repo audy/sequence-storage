59451211c2adfbc6b216d2a11e332baa0b5e9024
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-02-22 12:23:41 -0500

    routes should be called controllers

--- application.rb
+++ application.rb
@@ -1,7 +1,7 @@
 require './environment.rb'
 require './helpers.rb'
 
-Dir['./routes/*'].each { |f| require f }
+Dir['./controllers/*'].each { |f| require f }
 
 before do
   @user = User.get(session[:user_id]) if session[:user_id]
