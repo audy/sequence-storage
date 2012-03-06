fee1bc9731dbf105c5b0d186fa9a7c3cdbd4fd3d
Yushun <sonnyhe2002@gmail.com>
2012-03-06 17:07:44 -0500

    fix chksum not match and data creates dif experiment for dif files

--- data.rb
+++ data.rb
@@ -12,8 +12,6 @@ puts "\n"
 bucket = AWS::S3::Bucket.find(BUCKET_NAME)
 baby = bucket.objects
 
-experiment = nil   # need so experiment won't be deallocated when looping
-
 num_files_created = 0
 num_dataset_missing = 0
 num_dataset_here = 0
@@ -29,49 +27,46 @@ end
 
 baby.each do |b|
 
-    filename = b.path.gsub("/#{BUCKET_NAME}/", "")
-    
-    # Check if the dataset is in s3, create it, if not.
-    if !Dataset.first(:path => filename)
-      
-      size = b.size # gets file size from s3
-      
-      # file has to be greater than zero, so directorys will not be created as datasets
-      if size.to_i > 0
-        
-        # if it is the first missing, then create a new experiment
-        if num_dataset_missing == 0
-          experiment = Experiment.first_or_create({:name => BUCKET_NAME},{:description => "Created on #{Time.now} by #{user.name} for #{BUCKET_NAME}"})
-          if experiment.users.first.nil?
-            user.experiments << experiment
-            user.save
-            puts "#{experiment.name} linked to #{user.name}"
-          end
-          break if !experiment
-        end
-        
-        # create dataset
-        unparsed_chksum  = b.about().to_a[4][1]
-        
-        file = Dataset.new
-        file.path   = filename
-        file.size   = size
-        file.mdsum  = unparsed_chksum[3..unparsed_chksum.length]  # get checksum from s3,
-        file.experiment = experiment
-        file.user = user
-        
-        if file.save 
-          puts "#{filename} created"
-          num_files_created = num_files_created + 1
-        else
-          puts "error: #{filename} could not be created"
-        end
-        num_dataset_missing = num_dataset_missing + 1
+  filename = b.path.gsub("/#{BUCKET_NAME}/", "")
+  dirname = File.dirname(filename)
+  # Check if the dataset is in s3, create it, if not.
+  if !Dataset.first(:path => filename)
+
+    size = b.size # gets file size from s3
+
+    # file has to be greater than zero, so directorys will not be created as datasets
+    if size.to_i > 0
+
+      experiment = Experiment.first_or_create({:name => dirname},{:description => "Created on #{Time.now} by #{user.name}"})
+      if experiment.users.first.nil?
+        user.experiments << experiment
+        user.save
+        puts "#{experiment.name} linked to #{user.name}"
       end
-    else
-      puts "Here!  #{filename}"
-      num_dataset_here = num_dataset_here + 1
+      break if !experiment
+        
+      # create dataset
+      chksum  = b.about['etag'].tr('"','')
+      
+      file = Dataset.new
+      file.path   = filename
+      file.size   = size
+      file.mdsum  = chksum  # get checksum from s3,
+      file.experiment = experiment
+      file.user = user
+        
+      if file.save 
+        puts "#{filename} created"
+        num_files_created = num_files_created + 1
+      else
+        puts "error: #{filename} could not be created"
+      end
+      num_dataset_missing = num_dataset_missing + 1
     end
+  else
+    puts "Here!  #{filename}"
+    num_dataset_here = num_dataset_here + 1
+  end
 end
 
 puts "\nThe number of datasets missing were #{num_dataset_missing}"
